0fc54cf27d08eef5a3276581a00fe6dc7567cfba
Saqibur <saqibur.rahman98@gmail.com>
2020-08-13 00:33:08 +0600

    Add tests to main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+from cli import check_files
+from cli import wiki_tester
 
 if __name__ == "__main__":
-    print("You did not do anything yet...")
+    # check_files.all_files()
+    wiki_tester.test()
