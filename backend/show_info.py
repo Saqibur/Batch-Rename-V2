cc57803988bde700b527631df2b98e8be0d15e76
Saqibur <saqibur.rahman98@gmail.com>
2020-08-15 12:23:13 +0600

    Revamp backend searcher

--- backend/show_info.py
+++ backend/show_info.py
@@ -1,6 +1,13 @@
-import wikipedia
+from jikanpy import Jikan
+from config import config
+from classes.Anime import Anime
 
-def testing():
-    # print(wikipedia.summary("Wikipedia"))
-    # print(wikipedia.summary("korewas zombie desu"))
-    # print(wikipedia.)
+def search(search_term: str, best_matches=5) -> dict:
+    jikan = Jikan()
+    results = jikan.search(
+                search_type=config.SEARCH_TYPE,
+                query=search_term
+            )
+    results = results["results"][:best_matches]
+
+    return [ Anime(result) for result in results ]
