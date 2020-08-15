07b121f97833a7519d9a9647ec4171aa37e939f1
Saqibur <saqibur.rahman98@gmail.com>
2020-08-15 15:03:22 +0600

    Add minimally functional main

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from cli import wiki_tester
 from cli import anime_searcher
 from cli import renamer
 
+import webbrowser
+
 if __name__ == "__main__":
 
-    path_to_anime = str(input("Path to folder?"))
+    path_to_anime = str(input("Path to folder: "))
     all_files = check_files.all_files(path_to_anime)
     # wiki_tester.test()
     top_anime_results = anime_searcher.search()
@@ -18,3 +20,11 @@ if __name__ == "__main__":
     selected_anime = top_anime_results[selection - 1]
 
     renamer.show_before_after(selected_anime, all_files)
+
+    confirmation = str(input("Do you want to rename your files? (y/n) ")).lower()
+    if confirmation == "y":
+        print("Changing file names")
+        renamer.rename_files(selected_anime, all_files)
+        webbrowser.open(path_to_anime)
+    else:
+        print("Not changing anything. Exiting now.")
