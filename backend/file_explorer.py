fde71e9533a86b3048560f70a1f679e740456a03
Saqibur <saqibur.rahman98@gmail.com>
2020-08-15 15:03:34 +0600

    Add renaming methods to file explorer

--- backend/file_explorer.py
+++ backend/file_explorer.py
@@ -14,3 +14,8 @@ def all_files_in_path(path: str) -> list:
             all_files_in_folder.append(os.path.join(path, name))
 
     return all_files_in_folder
+
+def rename_file(path, new_name):
+    basename = os.path.basename(path)
+    extention = os.path.splitext(path)
+    os.rename(path, path.split(basename)[0] + new_name + extention[1])
