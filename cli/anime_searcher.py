c5356a5d0ec6e1889a2e3fc158c1043639e5fe61
Saqibur <saqibur.rahman98@gmail.com>
2020-08-15 12:23:00 +0600

    Add search wrapper which retrieves Anime objects

---
+++ cli/anime_searcher.py
@@ -0,0 +1,12 @@
+from backend import show_info
+import json
+
+def search(best_matches=5):
+    # best matches = 5 means return the top 5 results.
+    search_term = input("Show name: ")
+    print("Fetching results, please wait...")
+    result = show_info.search(str(search_term), best_matches)
+
+    print("Retrieved: %d shows.\n" % len(result))
+    for res in result:
+        print(res.title)
