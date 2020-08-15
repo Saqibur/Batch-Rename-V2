2e0304e9cc0ea39429e54881a4d7e79fecc828e6
Saqibur <saqibur.rahman98@gmail.com>
2020-08-15 13:51:33 +0600

    Change returns for all files func

--- cli/check_files.py
+++ cli/check_files.py
@@ -5,8 +5,9 @@ import colorama
 ### FOR REASONS ###
 colorama.init()
 
-def all_files():
-    all_files = file_explorer.all_files_in_path(config.TESTING_FOLDER_PATH)
+def all_files(path=config.TESTING_FOLDER_PATH):
+    all_files = file_explorer.all_files_in_path(path)
     print("There are %d files" % len(all_files))
-    for file in all_files:
-        print(file)
+    return all_files
+    # for file in all_files:
+      #   print(file)
