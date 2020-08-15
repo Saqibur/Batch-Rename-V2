8d5d0222c146442edfe0f1b6a3a4b2f04edb32bc
Saqibur <saqibur.rahman98@gmail.com>
2020-08-15 15:03:45 +0600

    Handle api exceptions

--- backend/show_info.py
+++ backend/show_info.py
@@ -2,31 +2,43 @@ from jikanpy import Jikan
 from config import config
 from classes.Anime import Anime
 from classes.Episode import Episode
+from jikanpy.exceptions import APIException
 
 def __set_anime_episodes(anime_id: int) -> list:
     print("Fetching episode lists for: %d" % anime_id)
 
     jikan = Jikan()
-    anime_with_episodes = jikan.anime(anime_id, extension='episodes')['episodes']
+    try:
+        anime_with_episodes = jikan.anime(anime_id, extension='episodes')['episodes']
+        episodes = []
 
-    episodes = []
-
-    for ep in anime_with_episodes:
-        episodes.append(
-            Episode(
-                ep['episode_id'],
-                ep['title'],
+        for ep in anime_with_episodes:
+            episodes.append(
+                Episode(
+                    ep['episode_id'],
+                    ep['title'],
+                )
             )
-        )
 
-    return episodes
+        return episodes
+    except APIException:
+        print("Failed to retrieve and connect to MAL.")
+        print("Please close the app and try again later")
+    except:
+        print("SOMETHING WENT REALLY BAD")
 
 def search(search_term: str, best_matches=5) -> dict:
     jikan = Jikan()
-    results = jikan.search(
-                search_type=config.SEARCH_TYPE,
-                query=search_term
-            )
+    try:
+        results = jikan.search(
+                    search_type=config.SEARCH_TYPE,
+                    query=search_term
+                )
+    except APIException:
+        print("Failed to retrieve and connect to MAL.")
+    except:
+        print("Something went horribly wrong. You should probably close the application. Lol.")
+
     results = results["results"][:best_matches]
 
     retrieved_anime = list()
