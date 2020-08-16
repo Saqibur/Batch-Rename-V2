6d45df66e2b54ba1e669ee46ee3a9c03fd6a934f
Saqibur <saqibur.rahman98@gmail.com>
2020-08-16 18:36:19 +0600

    Remove checking message

--- backend/network.py
+++ backend/network.py
@@ -3,10 +3,10 @@ import logging
 import urllib.error
 
 def internet_on():
-    print("Checking your internet connection.")
+    # print("Checking your internet connection.")
     try:
         urllib.request.urlopen('https://myanimelist.net/', timeout=2)
         return True
     except urllib.error.URLError as err:
-        logging.error(err)
+        # logging.error(err)
         return False
