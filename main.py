89ca5a5a12e98e3b7691bbde24ee527c575ebf49
Saqibur <saqibur.rahman98@gmail.com>
2020-08-16 17:51:35 +0600

    Add folder checks for logging

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from cli import renamer
 from config import config
 
 import logging
-
-logging.basicConfig(
-    filename=config.LOG_FILE,
-    filemode='w',
-    format='%(asctime)s.%(msecs)03d %(levelname)s - %(message)s',
-    level=logging.INFO,
-    datefmt="%Y-%m-%d %H:%M:%S"
-)
-
 import webbrowser
+import os
 
 if __name__ == "__main__":
+
+    if os.path.isdir(config.LOG_FOLDER) is not True:
+        os.mkdir(config.LOG_FOLDER)
+
+    logging.basicConfig(
+        filename=config.LOG_FILE,
+        filemode='w+',
+        format='%(asctime)s.%(msecs)03d %(levelname)s - %(message)s',
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
     logging.info("Application Started")
     print(config.WELCOME_MESSAGE)
     path_to_anime = str(input("Path to folder: "))
