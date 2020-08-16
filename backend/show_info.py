c7fe66d716c0886951e74856e43d0d4d134df29c
Saqibur <saqibur.rahman98@gmail.com>
2020-08-16 17:41:59 +0600

    Add api failure logs

--- backend/show_info.py
+++ backend/show_info.py
@@ -3,6 +3,7 @@ from config import config
 from classes.Anime import Anime
 from classes.Episode import Episode
 from jikanpy.exceptions import APIException
+import logging
 
 def __set_anime_episodes(anime_id: int) -> list:
     print("Fetching episode lists for: %d" % anime_id)
@@ -22,9 +23,11 @@ def __set_anime_episodes(anime_id: int) -> list:
 
         return episodes
     except APIException:
+        logging.error("Failed to retrieve and connect to MAL when fetching episode titles.")
         print("Failed to retrieve and connect to MAL.")
         print("Please close the app and try again later")
     except:
+        logging.error("SOMETHING WENT REALLY BAD")
         print("SOMETHING WENT REALLY BAD")
 
 def search(search_term: str, best_matches=5) -> dict:
@@ -36,6 +39,7 @@ def search(search_term: str, best_matches=5) -> dict:
                 )
     except APIException:
         print("Failed to retrieve and connect to MAL.")
+        logging.error("Failed to retrieve and connect to MAL when searching.")
     except:
         print("Something went horribly wrong. You should probably close the application. Lol.")
 
